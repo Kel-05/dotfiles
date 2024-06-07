7770c6b70655192a1a057bc5974d08bcd1ca63d6
Kel <97975851+KelAnims@users.noreply.github.com>
2024-06-07 19:27:59 +0200

    Update emacs-daemon.sh

--- hypr/scripts/exec-once.d/emacs-daemon.sh
+++ hypr/scripts/exec-once.d/emacs-daemon.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-icon=~/.local/share/icons/Tela-circle-blue-dark/scalable/apps/emacs.svg
-error=~/.local/share/icons/Catppuccin-Frappe/status/22/dialog-error.svg
 
-notify-send -i $icon -a Emacs "Emacs" "Starting Emacs daemon..."
-emacs --daemon
-if [ $? -eq 0 ]; then
-    notify-send -i $icon -a Emacs "Emacs" "Emacs daemon started successfully!"
-else
-    notify-send -i $error -a Emacs "Error: Emacs" "An error happened while starting Emacs."
+if ! pidof emacs; then
+    icon=~/.local/share/icons/Tela-circle-blue-dark/scalable/apps/emacs.svg
+    error=~/.local/share/icons/Catppuccin-Frappe/status/22/dialog-error.svg
+    
+    notify-send -i $icon -a Emacs "Emacs" "Starting Emacs daemon..."
+    emacs --daemon
+    if [ $? -eq 0 ]; then
+	notify-send -i $icon -a Emacs "Emacs" "Emacs daemon started successfully!"
+    else
+	notify-send -i $error -a Emacs "Error: Emacs" "An error happened while starting Emacs."
+    fi
 fi
