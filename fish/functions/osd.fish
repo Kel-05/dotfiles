1e30f3c3e9d273741fbb1b0838ed4811a4ea49e2
Kel <97975851+KelAnims@users.noreply.github.com>
2024-06-15 01:31:47 +0200

    Chore: remove osd.fish

--- fish/functions/osd.fish
+++
@@ -1,4 +0,0 @@
-function osd --wraps='sudo systemctl start swayosd-libinput-backend.service && swayosd-server &; disown %1' --description 'alias osd=sudo systemctl start swayosd-libinput-backend.service && swayosd-server &; disown %1'
-  sudo systemctl start swayosd-libinput-backend.service && swayosd-server &; disown %1 $argv
-        
-end
