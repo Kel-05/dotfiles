e2e74fa97c8f3d1aa7a3111afab3c6189cf8960b
Kel <97975851+KelAnims@users.noreply.github.com>
2024-06-18 13:26:26 +0200

    Chore(Fish): removed unused variables

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     eval (ssh-agent -c) > /dev/null
-    set SHELL_MOMMYS_LITTLE "boy"
-    set SHELL_MOMMYS_ONLY_NEGATIVE "true"
     set -g man_bold -o 8caaee
     set -g man_standout -b c6d0f5 303446
 end
