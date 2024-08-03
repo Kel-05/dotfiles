e7a524ca9c1d665d1936017078f3ba9b8ecd5315
Kel <97975851+KelAnims@users.noreply.github.com>
2024-08-03 08:40:24 +0200

    Misc: minor changes

--- hypr/scripts/disable-while-typing.sh
+++ hypr/scripts/disable-while-typing.sh
@@ -2,10 +2,12 @@
 time=3000
 icon=~/.local/share/icons/Catppuccin-Frappe/status/22/dialog-information.svg
 
-if grep 1 <<< "$(hyprctl getoption input:touchpad:disable_while_typing)"; then
+if hyprctl getoption input:touchpad:disable_while_typing | grep 1; then
     hyprctl keyword input:touchpad:disable_while_typing false
+    hyprctl keyword cursor:hide_on_key_press false
     notify-send -i $icon -a Hyprland -t $time "Hyprland" "Touchpad will stay active when typing."
 else
     hyprctl keyword input:touchpad:disable_while_typing true
+    hyprctl keyword cursor:hide_on_key_press true
     notify-send -i $icon -a Hyprland -t $time "Hyprland" "Touchpad will be disabled when typing."
 fi
--- hypr/scripts/layout.sh
+++ hypr/scripts/layout.sh
@@ -2,7 +2,7 @@
 time=3000
 icon=~/.local/share/icons/Catppuccin-Frappe/status/22/dialog-information.svg
 
-if grep dwindle <<< "$(hyprctl getoption general:layout)"; then
+if hyprctl getoption general:layout | grep dwindle; then
     hyprctl keyword general:layout master
     notify-send -i $icon -a Hyprland -t $time "Hyprland" "Layout set to master"
 else
