427feedc9479c5ebe534b3b652ad9b62e08e6066
Kel <97975851+KelAnims@users.noreply.github.com>
2024-08-16 00:49:22 +0200

    Hyprland: better zoom and new keybinds

--- hypr/scripts/zoom.sh
+++ hypr/scripts/zoom.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
+zoom_value=$(hyprctl getoption cursor:zoom_factor | grep [0-9] | cut -d" " -f2) 
 
-if hyprctl getoption cursor:zoom_factor | grep 1; then
-    hyprctl keyword cursor:zoom_factor 3
-else
-    hyprctl keyword cursor:zoom_factor 1
-fi
+zoom_value=$(echo "$zoom_value $1 0.2" | bc)
+hyprctl keyword cursor:zoom_factor $zoom_value
