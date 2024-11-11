c77c49ab51034d13ee4e857ca5486f301a858308
Kel <97975851+KelAnims@users.noreply.github.com>
2024-11-11 09:35:19 +0100

    Themes: refactor install.sh

--- hypr/themes/catppuccin-frappe-blue/install.sh
+++ hypr/themes/catppuccin-frappe-blue/install.sh
@@ -14,40 +14,37 @@ icons_index="$HOME/.local/share/icons/default/index.theme"
 
 fish -c "set -Ux HYPRLAND_THEME ${theme##*/}"
 
-cp $theme/theme.conf $hyprland/theme.conf
-cp $theme/theme.el $emacs/theme.el
+cp $theme/theme.conf             $hyprland/theme.conf
+cp $theme/theme.el               $emacs/theme.el
+cp $theme/gtk-3.0-settings.ini   $gtk3/settings.ini
+cp $theme/gtk-4.0-settings.ini   $gtk4/settings.ini
 
-emacsclient --eval "(load-theme 'catppuccin :no-confirm)"
-emacsclient --eval "(catppuccin-load-flavor 'frappe)"
+cp $theme/icons-index.theme      $icons_index
+cp $theme/colorscheme.css        $waybar/colorscheme.css
+cp $theme/swaync-style.css       $swaync/style.css
+cp $theme/konsole.colorscheme    $konsole/konsole.colorscheme
 
-cp $theme/gtk-3.0-settings.ini $gtk3/settings.ini
-cp $theme/gtk-4.0-settings.ini $gtk4/settings.ini
-cp $theme/icons-index.theme $icons_index
+cp $theme/gitui-theme.ron        $gitui/theme.ron
+cp $theme/kitty-bg.conf          $kitty/kitty-bg.conf
+cp $theme/micro-settings.json    $micro/settings.json
+cp $theme/wallpaper              $hyprland/assets/wallpaper
 
-gsettings set org.gnome.desktop.interface gtk-theme \'catppuccin-frappe-blue-standard+default\'
-gsettings set org.gnome.desktop.interface cursor-theme \'catppuccin-frappe-blue-cursors\'
-gsettings set org.gnome.desktop.interface icon-theme \'Tela-circle-blue-dark\'
+gsettings set org.gnome.desktop.interface gtk-theme       \'catppuccin-frappe-blue-standard+default\'
+gsettings set org.gnome.desktop.interface cursor-theme    \'catppuccin-frappe-blue-cursors\'
+gsettings set org.gnome.desktop.interface icon-theme      \'Tela-circle-blue-dark\'
 
-cp $theme/colorscheme.css $waybar/colorscheme.css
-cp $theme/swaync-style.css $swaync/style.css
 swaync-client -rs
-
 pkill waybar
 waybar &
 
 plasma-apply-colorscheme CatppuccinFrappeBlue
-
-cp $theme/konsole.colorscheme $konsole/konsole.colorscheme
-cp $theme/gitui-theme.ron $gitui/theme.ron
-cp $theme/kitty-bg.conf $kitty/kitty-bg.conf
-
 fish -c "yes | fish_config theme save \"Catppuccin Frappe\""
-
-cp $theme/micro-settings.json $micro/settings.json
-cp $theme/wallpaper $hyprland/assets/wallpaper
 
 if ! echo catppuccin-frappe-blue | grep $(hyprctl hyprpaper listloaded) > /dev/null; then
     hyprctl hyprpaper preload $theme/wallpaper
 fi
 
 hyprctl hyprpaper wallpaper , $theme/wallpaper
+
+emacsclient --eval "(load-theme 'catppuccin :no-confirm)"
+emacsclient --eval "(catppuccin-load-flavor 'frappe)"
--- hypr/themes/catppuccin-latte-red/install.sh
+++ hypr/themes/catppuccin-latte-red/install.sh
@@ -14,40 +14,37 @@ icons_index="$HOME/.local/share/icons/default/index.theme"
 
 fish -c "set -Ux HYPRLAND_THEME ${theme##*/}"
 
-cp $theme/theme.conf $hyprland/theme.conf
-cp $theme/theme.el $emacs/theme.el
+cp $theme/theme.conf             $hyprland/theme.conf
+cp $theme/theme.el               $emacs/theme.el
+cp $theme/gtk-3.0-settings.ini   $gtk3/settings.ini
+cp $theme/gtk-4.0-settings.ini   $gtk4/settings.ini
 
-emacsclient --eval "(load-theme 'catppuccin :no-confirm)"
-emacsclient --eval "(catppuccin-load-flavor 'latte)"
+cp $theme/icons-index.theme      $icons_index
+cp $theme/colorscheme.css        $waybar/colorscheme.css
+cp $theme/swaync-style.css       $swaync/style.css
+cp $theme/konsole.colorscheme    $konsole/konsole.colorscheme
 
-cp $theme/gtk-3.0-settings.ini $gtk3/settings.ini
-cp $theme/gtk-4.0-settings.ini $gtk4/settings.ini
-cp $theme/icons-index.theme $icons_index
+cp $theme/gitui-theme.ron        $gitui/theme.ron
+cp $theme/kitty-bg.conf          $kitty/kitty-bg.conf
+cp $theme/micro-settings.json    $micro/settings.json
+cp $theme/wallpaper              $hyprland/assets/wallpaper
 
-gsettings set org.gnome.desktop.interface gtk-theme \'catppuccin-latte-red-standard+default\'
-gsettings set org.gnome.desktop.interface cursor-theme \'catppuccin-latte-red-cursors\'
-gsettings set org.gnome.desktop.interface icon-theme \'Tela-circle-red-light\'
+gsettings set org.gnome.desktop.interface gtk-theme       \'catppuccin-latte-red-standard+default\'
+gsettings set org.gnome.desktop.interface cursor-theme    \'catppuccin-latte-red-cursors\'
+gsettings set org.gnome.desktop.interface icon-theme      \'Tela-circle-red-light\'
 
-cp $theme/colorscheme.css $waybar/colorscheme.css
+swaync-client -rs
 pkill waybar
 waybar &
 
-cp $theme/swaync-style.css $swaync/style.css
-swaync-client -rs
-
 plasma-apply-colorscheme CatppuccinLatteRed
-
-cp $theme/konsole.colorscheme $konsole/konsole.colorscheme
-cp $theme/gitui-theme.ron $gitui/theme.ron
-cp $theme/kitty-bg.conf $kitty/kitty-bg.conf
-
 fish -c "yes | fish_config theme save \"Catppuccin Latte\""
-
-cp $theme/micro-settings.json $micro/settings.json
-cp $theme/wallpaper $hyprland/assets/wallpaper
 
 if ! echo catppuccin-latte-red | grep $(hyprctl hyprpaper listloaded) > /dev/null; then
     hyprctl hyprpaper preload $theme/wallpaper
 fi
 
 hyprctl hyprpaper wallpaper , $theme/wallpaper
+
+emacsclient --eval "(load-theme 'catppuccin :no-confirm)"
+emacsclient --eval "(catppuccin-load-flavor 'latte)"
