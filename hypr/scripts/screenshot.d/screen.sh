5a9075442988084eadba8941717b49edbda5fa76
Kel <97975851+KelAnims@users.noreply.github.com>
2024-12-04 11:41:26 +0100

    Grimblast: swap time and date in file format

--- hypr/scripts/screenshot.d/screen.sh
+++ hypr/scripts/screenshot.d/screen.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-dir=~/Pictures/Screenshots/screenshot_$(date +%H.%M.%S_%d-%m-%Y).png
+dir=~/Pictures/Screenshots/screenshot_$(date +%d-%m-%Y_%H.%M.%S).png
 grimblast --notify copysave screen $dir
