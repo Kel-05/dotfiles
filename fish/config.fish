82b426f2c5cc00a5fe457e270b15b6c607917868
Kel <97975851+KelAnims@users.noreply.github.com>
2025-04-20 18:35:11 +0200

    Fish: remove unused env variable

--- fish/config.fish
+++ fish/config.fish
@@ -3,7 +3,6 @@ if status is-interactive
     eval (ssh-agent -c) > /dev/null
     set -g man_bold -o 8caaee
     set -g man_standout -b c6d0f5 303446
-    set -x MICRO_TRUECOLOR 1
     fish_add_path ~/.local/bin
     fish_add_path ~/.dotnet/tools
 end
